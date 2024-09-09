a8bb0e4c3ff742a27f19ca0d9477f469ac32fb22
jeniaainej080731 <jeniaainej080731@gmail.com>
2024-09-09 21:55:35 +0300

    .

--- src/main/java/com/example/LibraryForSummer/LibrarySpringBoodApplication.java
+++ src/main/java/com/example/LibraryForSummer/LibrarySpringBoodApplication.java
@@ -20,46 +20,46 @@ public class LibrarySpringBoodApplication {
 	public CommandLineRunner insertData(GenresRepository genreRepository, TypesRepository typeRepository) {
 		return args -> {
 			// Добавляем данные в таблицу Genre
-			genreRepository.save(new Genre("Fiction", null));
-			genreRepository.save(new Genre("Non-Fiction", null));
-			genreRepository.save(new Genre("Science Fiction", null));
-			genreRepository.save(new Genre("Fantasy", null));
-			genreRepository.save(new Genre("Mystery", null));
-			genreRepository.save(new Genre("Thriller", null));
-			genreRepository.save(new Genre("Romance", null));
-			genreRepository.save(new Genre("Historical", null));
-			genreRepository.save(new Genre("Biography", null));
-			genreRepository.save(new Genre("Self-Help", null));
-			genreRepository.save(new Genre("Cookbook", null));
-			genreRepository.save(new Genre("Travel", null));
-			genreRepository.save(new Genre("Science", null));
-			genreRepository.save(new Genre("Childrens", null));
-			genreRepository.save(new Genre("Young Adult", null));
-			genreRepository.save(new Genre("Horror", null));
-			genreRepository.save(new Genre("Classic", null));
-			genreRepository.save(new Genre("Poetry", null));
-			genreRepository.save(new Genre("Adventure", null));
-			genreRepository.save(new Genre("Graphic Novel", null));
-			genreRepository.save(new Genre("Memoir", null));
-			genreRepository.save(new Genre("Philosophy", null));
-			genreRepository.save(new Genre("Religion", null));
-			genreRepository.save(new Genre("Health", null));
-			genreRepository.save(new Genre("Business", null));
-			genreRepository.save(new Genre("Politics", null));
-			genreRepository.save(new Genre("Psychology", null));
-			genreRepository.save(new Genre("Education", null));
-			genreRepository.save(new Genre("Art", null));
-			genreRepository.save(new Genre("Sports", null));
-			genreRepository.save(new Genre("Music", null));
-			genreRepository.save(new Genre("Technology", null));
+			genreRepository.save(new Genre("Fiction"));
+			genreRepository.save(new Genre("Non-Fiction"));
+			genreRepository.save(new Genre("Science Fiction"));
+			genreRepository.save(new Genre("Fantasy"));
+			genreRepository.save(new Genre("Mystery"));
+			genreRepository.save(new Genre("Thriller"));
+			genreRepository.save(new Genre("Romance" ));
+			genreRepository.save(new Genre("Historical"));
+			genreRepository.save(new Genre("Biography"));
+			genreRepository.save(new Genre("Self-Help"));
+			genreRepository.save(new Genre("Cookbook"));
+			genreRepository.save(new Genre("Travel"));
+			genreRepository.save(new Genre("Science"));
+			genreRepository.save(new Genre("Childrens"));
+			genreRepository.save(new Genre("Young Adult"));
+			genreRepository.save(new Genre("Horror"));
+			genreRepository.save(new Genre("Classic"));
+			genreRepository.save(new Genre("Poetry"));
+			genreRepository.save(new Genre("Adventure"));
+			genreRepository.save(new Genre("Graphic Novel"));
+			genreRepository.save(new Genre("Memoir"));
+			genreRepository.save(new Genre("Philosophy"));
+			genreRepository.save(new Genre("Religion"));
+			genreRepository.save(new Genre("Health"));
+			genreRepository.save(new Genre("Business"));
+			genreRepository.save(new Genre("Politics"));
+			genreRepository.save(new Genre("Psychology"));
+			genreRepository.save(new Genre("Education"));
+			genreRepository.save(new Genre("Art"));
+			genreRepository.save(new Genre("Sports"));
+			genreRepository.save(new Genre("Music"));
+			genreRepository.save(new Genre("Technology"));
 
 			// Добавляем данные в таблицу Type
-			typeRepository.save(new Type("Educational", null));
-			typeRepository.save(new Type("Popular science", null));
-			typeRepository.save(new Type("Scientific", null));
-			typeRepository.save(new Type("Reference", null));
-			typeRepository.save(new Type("Fiction", null));
-			typeRepository.save(new Type("Non-Fiction", null));
+			typeRepository.save(new Type("Educational"));
+			typeRepository.save(new Type("Popular science"));
+			typeRepository.save(new Type("Scientific"));
+			typeRepository.save(new Type("Reference"));
+			typeRepository.save(new Type("Fiction"));
+			typeRepository.save(new Type("Non-Fiction"));
 		};
 	}
 }
